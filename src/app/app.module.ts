86d15ff2e1fbdc0543412b659089837684649229
CHOI <frank.choi@bycn.bouygues-construction.com>
2018-11-09 17:49:47 +0800

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
+
+import { AppRoutes } from './app.routes';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { ImageFilterPipe } from './image/shared/filter.pipe';
+import { ImageService } from './image/shared/image.service';
+import { AuthService } from './services/auth.service';
+
+
+
+import { AboutComponent } from './about/about.component';
+import { BookingComponent } from './booking/booking.component';
+import { ContactComponent } from './contact/contact.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageComponent } from './image/image.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisteruserComponent } from './registeruser/registeruser.component';
+import { SchedulerComponent } from './scheduler/scheduler.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    BookingComponent,
+    ContactComponent,
+    DashboardComponent,
+    GalleryComponent,
+    ImageComponent,
+    LoginComponent,
+    NavbarComponent,
+    RegisteruserComponent,
+    SchedulerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [];
+
+export const AppRoutes = RouterModule.forRoot(appRoutes);
